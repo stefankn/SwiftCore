8e2cea6a69fa28363493b6806988dd52f07781d2
Stefan <1708541+stefankn@users.noreply.github.com>
2022-12-28 10:30:28 +0100

    Package requirements

--- Package.swift
+++ Package.swift
@@ -5,11 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftCore",
+    platforms: [
+        .macOS(.v13),
+        .iOS(.v16)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftCore",
-            targets: ["SwiftCore"]),
+            targets: ["SwiftCore"]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,9 +25,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftCore",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "SwiftCoreTests",
-            dependencies: ["SwiftCore"]),
+            dependencies: ["SwiftCore"]
+        )
     ]
 )
