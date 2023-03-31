7009a97cb4e6a143011301fbc279f951088f4a3c
Stefan <1708541+stefankn@users.noreply.github.com>
2023-03-31 17:45:27 +0200

    Array extension

---
+++ Sources/SwiftCore/Extensions/Array+Utilities.swift
@@ -0,0 +1,17 @@
+//
+//  Array+Utilities.swift
+//  Linnet
+//
+//  Created by Stefan Klein Nulent on 19/01/2023.
+//
+
+import Foundation
+
+extension Array {
+    
+    // MARK: - Properties
+    
+    public var isNotEmpty: Bool {
+        !isEmpty
+    }
+}
